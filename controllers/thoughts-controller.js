faada61e7a20b103f6742c7465c78f217de35eb7
geocode-matt <mlindenx@gmail.com>
2020-09-18 12:00:08 -0500

    started users-controller.js

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,5 +1,14 @@
 const { Thoughts, Users } = require('../models');
 
+// THOUGHT ROUTES //
+// get all thoughts
+// get a thought by _id
+// create a new thought
+// update a thought
+// delete a thought
+// create a reaction to a thought
+// delete a reaction from a thought
+
 const thoughtsController = {
 
     // 
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,7 +1,27 @@
 const { Users } = require('../models');
 
+// USER ROUTES //
+// get all users
+// get a user by its _id, populate thoughts and friends data
+// create a new user
+// update a user
+// delete a user
+// add a friend to a user's friend list
+// delete a friend from a user's friend list
+
 const usersController = {
 
-    // 
+    // Get all users
+    getAllUsers(req, res) {
+        Users.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+    }
 
 }
