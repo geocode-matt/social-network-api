6d87a38ea5ef6bffbbf433b35eb07f921bb7845e
geocode-matt <mlindenx@gmail.com>
2020-09-18 11:29:36 -0500

    both models built

---
+++ models/Thoughts.js
@@ -0,0 +1,69 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+          type: Date,
+          default: Date.now,
+          get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
+    }
+  );
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+  // get length of friends array
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+  // create the User model using UserSchema
+const Thoughts = model('Thoughts', ThoughtSchema);
+
+// export the User model
+module.exports = Thoughts;
